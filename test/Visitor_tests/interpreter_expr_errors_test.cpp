08e6e8caf8d52bf4426d16fe906df0e94f70396a
rozmarin <romaostafeychuk@gmail.com>
2026-02-11 14:41:16 +0300

    Add Interpreter runtime errors test

---
+++ test/Visitor_tests/interpreter_expr_errors_test.cpp
@@ -0,0 +1,55 @@
+#include "AST/AST.hpp"
+#include "Visitors/Interpreter.hpp"
+#include "gtest/gtest.h"
+
+TEST(InterpreterExprErrorTest, ExprNodeMissingExprTest) {
+    ast::Interpreter interpreter;
+    ast::ExprNode node;
+    EXPECT_THROW(node.accept(interpreter), std::runtime_error);
+}
+
+TEST(InterpreterExprErrorTest, UnOpNodeMissingOperandTest) {
+    ast::Interpreter interpreter;
+    ast::UnOpNode node(ast::unop_node_type::neg);
+    EXPECT_THROW(node.accept(interpreter), std::runtime_error);
+}
+
+TEST(InterpreterExprErrorTest, BinArithOpNodeMissingOperandTest) {
+    ast::Interpreter interpreter;
+
+    {
+        ast::BinArithOpNode node(
+            ast::bin_arith_op_type::add,
+            std::make_unique<ast::ValueNode>(1),
+            nullptr);
+        EXPECT_THROW(node.accept(interpreter), std::runtime_error);
+    }
+
+    {
+        ast::BinArithOpNode node(
+            ast::bin_arith_op_type::add,
+            nullptr,
+            std::make_unique<ast::ValueNode>(1));
+        EXPECT_THROW(node.accept(interpreter), std::runtime_error);
+    }
+}
+
+TEST(InterpreterExprErrorTest, BinLogicOpNodeMissingOperandTest) {
+    ast::Interpreter interpreter;
+
+    {
+        ast::BinLogicOpNode node(
+            ast::bin_logic_op_type::equal,
+            std::make_unique<ast::ValueNode>(1),
+            nullptr);
+        EXPECT_THROW(node.accept(interpreter), std::runtime_error);
+    }
+
+    {
+        ast::BinLogicOpNode node(
+            ast::bin_logic_op_type::equal,
+            nullptr,
+            std::make_unique<ast::ValueNode>(1));
+        EXPECT_THROW(node.accept(interpreter), std::runtime_error);
+    }
+}
