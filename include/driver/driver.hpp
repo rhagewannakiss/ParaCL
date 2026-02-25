5dcfab36290b026b2e22f93c265771dca80af98a
rhage <asya.makarska@gmail.com>
2026-02-25 21:23:26 +0300

    Fixes in driver and parser

--- include/driver/driver.hpp
+++ include/driver/driver.hpp
@@ -12,79 +12,90 @@
 #include "AST/AST.hpp"
 #include "driver/location_utils.hpp"
 
-namespace yy {
+namespace yy
+{
 
-class NumDriver {
-private:
-    FlexLexer* plex_;
-    location loc_;
-    ast::AST ast_;
-    int error_cnt_ = 0;
+    class NumDriver
+    {
+    private:
+        FlexLexer *plex_;
+        location loc_;
+        ast::AST ast_;
+        int error_cnt_ = 0;
 
-public:
-    explicit NumDriver(FlexLexer* plex) : plex_(plex) {}
+    public:
+        explicit NumDriver(FlexLexer *plex) : plex_(plex) {}
 
-    parser::token_type yylex(parser::semantic_type* yylval,
-                             parser::location_type* yylloc) {
-        loc_.step();
+        parser::token_type yylex(parser::semantic_type *yylval,
+                                 parser::location_type *yylloc)
+        {
+            loc_.step();
 
-        parser::token_type tt = static_cast<parser::token_type>(plex_->yylex());
+            parser::token_type tt = static_cast<parser::token_type>(plex_->yylex());
 
-        loc_.columns(plex_->YYLeng());
-        *yylloc = loc_;
+            loc_.columns(plex_->YYLeng());
+            *yylloc = loc_;
 
-        switch (tt) {
+            switch (tt)
+            {
             case parser::token_type::NUMBER:
-                yylval->emplace<int>(std::stoi(plex_->YYText()));
+                yylval->emplace<int64_t>(std::stoll(plex_->YYText()));
                 break;
             case parser::token_type::VAR:
                 yylval->emplace<std::string>(plex_->YYText());
                 break;
             default:
                 break;
+            }
+
+            return tt;
         }
 
-        return tt;
-    }
-
-    void add_error(const location& loc, const std::string& msg) {
-        auto range = to_source_range(loc);
-        if (!range.file.empty()) {
-            std::cerr << range.file << ":";
+        void add_error(const location &loc, const std::string &msg)
+        {
+            auto range = to_source_range(loc);
+            if (!range.file.empty())
+            {
+                std::cerr << range.file << ":";
+            }
+            std::cerr << "Error at " << loc.begin.line << ":" << loc.begin.column
+                      << ": " << msg << std::endl;
+            error_cnt_++;
         }
-        std::cerr << "Error at " << loc.begin.line << ":" << loc.begin.column
-                  << ": " << msg << std::endl;
-        error_cnt_++;
-    }
 
-    bool has_errors() const
-    {
-        return error_cnt_ > 0;
-    }
+        bool has_errors() const
+        {
+            return error_cnt_ > 0;
+        }
 
-    bool parse() {
-        parser parser(this);
-        bool res = parser.parse();
-        return !res;
-    }
+        bool parse()
+        {
+            parser parser(this);
+            bool res = parser.parse();
+            return !res;
+        }
 
-    void newline() {
-        loc_.lines(1);
-        loc_.step();
-    }
+        void newline()
+        {
+            loc_.lines(1);
+            loc_.step();
+        }
 
-    const location& get_location() const {
-        return loc_;
-    }
+        const location &get_location() const
+        {
+            return loc_;
+        }
 
-    void set_ast_root(std::unique_ptr<ast::BaseNode> root) {
-        ast_.set_root(std::move(root));
-    }
+        void set_ast_root(std::unique_ptr<ast::BaseNode> root)
+        {
+            ast_.set_root(std::move(root));
+        }
 
-    const ast::AST& get_ast() const {
-        return ast_;
-    }
-};
+        const ast::AST &get_ast() const
+        {
+            return ast_;
+        }
+    };
 
 } // namespace yy
 
