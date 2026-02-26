8a95723601e2d07c706dbabb7a0ae443eeb9f8df
rozmarin <romaostafeychuk@gmail.com>
2026-02-26 16:51:38 +0300

    changed tests for current InputNode contract

--- test/Visitor_tests/interpreter_control_flow_test.cpp
+++ test/Visitor_tests/interpreter_control_flow_test.cpp
@@ -17,6 +17,20 @@ std::string RunAndCapture(ast::BaseNode& node) {
     std::cout.rdbuf(old);
     return out.str();
 }
+
+class ScopedCinInput {
+public:
+    explicit ScopedCinInput(const std::string& input)
+      : in_(input), old_(std::cin.rdbuf(in_.rdbuf())) {}
+    ~ScopedCinInput() { std::cin.rdbuf(old_); }
+
+    ScopedCinInput(const ScopedCinInput&) = delete;
+    ScopedCinInput& operator=(const ScopedCinInput&) = delete;
+
+private:
+    std::istringstream in_;
+    std::streambuf* old_;
+};
 } // namespace
 
 TEST(InterpreterControlFlowTest, IfNodeTrueBranchTest) {
@@ -132,6 +146,96 @@ TEST(InterpreterControlFlowTest, WhileNodeMultipleIterationsTest) {
     EXPECT_EQ(RunAndCapture(root), "3\n");
 }
 
+TEST(InterpreterControlFlowTest, WhileConditionInputNodeIsReadOncePerNode) {
+    ScopedCinInput input("3\n1\n");
+
+    ast::ScopeNode root;
+    root.add_statement(std::make_unique<ast::VarDeclNode>(
+        "x",
+        std::make_unique<ast::ValueNode>(0)));
+
+    auto while_body = std::make_unique<ast::ScopeNode>();
+    while_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("x"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::add,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::ValueNode>(1))));
+
+    root.add_statement(std::make_unique<ast::WhileNode>(
+        std::make_unique<ast::BinLogicOpNode>(
+            ast::bin_logic_op_type::less,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::InputNode>()),
+        std::move(while_body)));
+    root.add_statement(std::make_unique<ast::PrintNode>(
+        std::make_unique<ast::VarNode>("x")));
+
+    EXPECT_EQ(RunAndCapture(root), "3\n");
+}
+
+TEST(InterpreterControlFlowTest, WhileConditionAssignTracksVariableValue) {
+    ast::ScopeNode root;
+    root.add_statement(std::make_unique<ast::VarDeclNode>(
+        "y",
+        std::make_unique<ast::ValueNode>(6)));
+
+    auto while_body = std::make_unique<ast::ScopeNode>();
+    while_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("x"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::ValueNode>(1))));
+    while_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("y"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("y"),
+            std::make_unique<ast::ValueNode>(2))));
+
+    root.add_statement(std::make_unique<ast::WhileNode>(
+        std::make_unique<ast::AssignNode>(
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::VarNode>("y")),
+        std::move(while_body)));
+    root.add_statement(std::make_unique<ast::PrintNode>(
+        std::make_unique<ast::VarNode>("y")));
+
+    EXPECT_EQ(RunAndCapture(root), "-6\n");
+}
+
+TEST(InterpreterControlFlowTest, WhileConditionVarDeclTracksVariableValue) {
+    ast::ScopeNode root;
+    root.add_statement(std::make_unique<ast::VarDeclNode>(
+        "y",
+        std::make_unique<ast::ValueNode>(6)));
+
+    auto while_body = std::make_unique<ast::ScopeNode>();
+    while_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("x"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::ValueNode>(1))));
+    while_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("y"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("y"),
+            std::make_unique<ast::ValueNode>(2))));
+
+    root.add_statement(std::make_unique<ast::WhileNode>(
+        std::make_unique<ast::VarDeclNode>(
+            "x",
+            std::make_unique<ast::VarNode>("y")),
+        std::move(while_body)));
+    root.add_statement(std::make_unique<ast::PrintNode>(
+        std::make_unique<ast::VarNode>("y")));
+
+    EXPECT_EQ(RunAndCapture(root), "-6\n");
+}
+
 TEST(InterpreterControlFlowTest, WhileNodeMissingConditionThrows) {
     ast::Interpreter interpreter;
     ast::WhileNode node(nullptr, std::make_unique<ast::ExprNode>(
@@ -203,6 +307,101 @@ TEST(InterpreterControlFlowTest, ForNodeZeroIterationsTest) {
         std::make_unique<ast::VarNode>("x")));
 
     EXPECT_EQ(RunAndCapture(root), "0\n");
+}
+
+TEST(InterpreterControlFlowTest, ForConditionInputNodeIsReadOncePerNode) {
+    ScopedCinInput input("4\n1\n");
+
+    ast::ScopeNode root;
+    root.add_statement(std::make_unique<ast::VarDeclNode>(
+        "x",
+        std::make_unique<ast::ValueNode>(0)));
+
+    root.add_statement(std::make_unique<ast::ForNode>(
+        std::make_unique<ast::AssignNode>(
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::ValueNode>(0)),
+        std::make_unique<ast::BinLogicOpNode>(
+            ast::bin_logic_op_type::less,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::InputNode>()),
+        std::make_unique<ast::AssignNode>(
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::BinArithOpNode>(
+                ast::bin_arith_op_type::add,
+                std::make_unique<ast::VarNode>("x"),
+                std::make_unique<ast::ValueNode>(1))),
+        std::make_unique<ast::ScopeNode>()));
+    root.add_statement(std::make_unique<ast::PrintNode>(
+        std::make_unique<ast::VarNode>("x")));
+
+    EXPECT_EQ(RunAndCapture(root), "4\n");
+}
+
+TEST(InterpreterControlFlowTest, ForConditionAssignTracksVariableValue) {
+    ast::ScopeNode root;
+    root.add_statement(std::make_unique<ast::VarDeclNode>(
+        "y",
+        std::make_unique<ast::ValueNode>(6)));
+
+    auto for_body = std::make_unique<ast::ScopeNode>();
+    for_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("x"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::ValueNode>(1))));
+    for_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("y"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("y"),
+            std::make_unique<ast::ValueNode>(2))));
+
+    root.add_statement(std::make_unique<ast::ForNode>(
+        nullptr,
+        std::make_unique<ast::AssignNode>(
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::VarNode>("y")),
+        nullptr,
+        std::move(for_body)));
+    root.add_statement(std::make_unique<ast::PrintNode>(
+        std::make_unique<ast::VarNode>("y")));
+
+    EXPECT_EQ(RunAndCapture(root), "-6\n");
+}
+
+TEST(InterpreterControlFlowTest, ForConditionVarDeclTracksVariableValue) {
+    ast::ScopeNode root;
+    root.add_statement(std::make_unique<ast::VarDeclNode>(
+        "y",
+        std::make_unique<ast::ValueNode>(6)));
+
+    auto for_body = std::make_unique<ast::ScopeNode>();
+    for_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("x"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("x"),
+            std::make_unique<ast::ValueNode>(1))));
+    for_body->add_statement(std::make_unique<ast::AssignNode>(
+        std::make_unique<ast::VarNode>("y"),
+        std::make_unique<ast::BinArithOpNode>(
+            ast::bin_arith_op_type::sub,
+            std::make_unique<ast::VarNode>("y"),
+            std::make_unique<ast::ValueNode>(2))));
+
+    root.add_statement(std::make_unique<ast::ForNode>(
+        nullptr,
+        std::make_unique<ast::VarDeclNode>(
+            "x",
+            std::make_unique<ast::VarNode>("y")),
+        nullptr,
+        std::move(for_body)));
+    root.add_statement(std::make_unique<ast::PrintNode>(
+        std::make_unique<ast::VarNode>("y")));
+
+    EXPECT_EQ(RunAndCapture(root), "-6\n");
 }
 
 TEST(InterpreterControlFlowTest, ForNodeMissingConditionThrows) {
